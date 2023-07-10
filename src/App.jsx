106d78896028442ded16116588aaf8dbc093b079
Saras <78587510+sarasverma@users.noreply.github.com>
2023-07-10 14:45:10 +0530

    intro section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
